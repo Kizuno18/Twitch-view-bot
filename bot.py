c83ab3246c2ad5c11c511a6ce07d5aa17c8459ad
coeex <71523355+coeex@users.noreply.github.com>
2022-08-14 15:15:58 +0200

    Episode 6
    Added the ability for the bot to look for proxies and use them.
    
    Also added the fail fix so we don't lose any bots we spawn.

--- bot.py
+++ bot.py
@@ -5,13 +5,18 @@ import time
 import pickle
 from os import listdir, getcwd
 from os.path import isfile,join
+import requests
 
 def BotLogic():
     while True:
         time.sleep(10)
 
-
-
+def GetProxyList(): # Make it so there is an option to read from a file.
+    proxy_list = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=5000&country=all&ssl=all&anonymity=all&simplified=true").content.decode("UTF-8")
+    proxy_list = proxy_list.split("\n")
+    for i in range(len(proxy_list)):
+        proxy_list[i] = proxy_list[i].rstrip()
+    return proxy_list
 
 def GetAllCookies():
     cookies = [f for f in listdir(getcwd()+"/tools/cookies/") if isfile(join(getcwd()+ "/tools/cookies/",f))]
@@ -19,9 +24,10 @@ def GetAllCookies():
 
 
 
-def SpawnBot(settings,cookie):
+def SpawnBot(settings,cookie,proxy):
     chrome_options = uc.ChromeOptions()
     chrome_options.add_argument("--mute-audio")
+    chrome_options.add_argument(f"--proxy-server={proxy}")
     driver = uc.Chrome(options=chrome_options)
     driver.get("https://www.twitch.tv/")
     logged_in = False
@@ -58,6 +64,5 @@ def SpawnBot(settings,cookie):
 
     driver.execute_script("document.getElementsByClassName('persistent-player')[0].style='';")
 
-
+    
     BotLogic()
-    #FIX THIS FOR THE VIEWERS
--- source.py
+++ source.py
@@ -11,17 +11,46 @@ settings = {}
 settings["streamer"] = config["default"]["streamer"]
 settings["bots"] = config["default"]["bots"]
 
+cookies = bot.GetAllCookies()
+proxies = bot.GetProxyList()
+bot_instances = []
 
-if __name__ == "__main__":
-    
-    cookies = bot.GetAllCookies()
-
-    for i in range(int(settings["bots"])):
+def BotStarter(id,cookie_override):
+    proxy = proxies[random.randint(0,len(proxies)-1)]
+    proxies.remove(proxy)
+    if cookie_override == None:
         if len(cookies) > 0:
             cookie = cookies[random.randint(0,len(cookies)-1)]
             cookies.remove(cookie)
-            process = multiprocessing.Process(target=bot.SpawnBot,args=(settings,cookie))
+
+            process = multiprocessing.Process(target=bot.SpawnBot,args=(settings,cookie,proxy))
         else:
-            process = multiprocessing.Process(target=bot.SpawnBot,args=(settings,None))
-        process.start()
+            process = multiprocessing.Process(target=bot.SpawnBot,args=(settings,None,proxy))
+            cookie = None
+            
+    else:
+        process = multiprocessing.Process(target=bot.SpawnBot,args=(settings,cookie_override,proxy))
+        cookie = cookie_override
+    process.start()
+    if cookie != None:
+        bot_instances.append([process,id,cookie])
+    else:
+        bot_instances.append([process,id,None])
+
+if __name__ == "__main__":
+    for i in range(int(settings["bots"])):
+        BotStarter(i,None)
+    
+    while True:
+        for i in range(int(settings["bots"])):
+            if bot_instances[i][0].is_alive() == False:
+                print("[!] Bot has died...")
+                bot_instances[i][0].close()
+                if bot_instances[i][2] != None:
+                    BotStarter(bot_instances[i][1],bot_instances[i][2])
+                else:
+                    BotStarter(bot_instances[i][1],None)
+                bot_instances.remove(bot_instances[i])
+
+        time.sleep(1)
 
